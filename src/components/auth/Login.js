a3b91e642008c604a358a76424c3cde3a2cd5fd0
skgilmore <skg.gilmore@gmail.com>
2021-02-01 10:30:35 -0600

    verified all merges and components up and running together

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -23,7 +23,7 @@ export const Login = props => {
             .then(exists => {
                 if (exists) {
                     localStorage.setItem("capstone_customer", exists.id)
-                    history.push("/")
+                    history.push("/cats")
                 } else {
                     existDialog.current.showModal()
                 }
