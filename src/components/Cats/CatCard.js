8e9de7054070a17b30a73f43e3c56ae88e76c948
skgilmore <skg.gilmore@gmail.com>
2021-02-06 23:57:26 -0600

    updated link to singular name

--- src/components/Cats/CatCard.js
+++ src/components/Cats/CatCard.js
@@ -11,10 +11,10 @@ export const CatCard = ({ cat }) => (
             <CardBody>
                 {/* <img src={cat.pic} alt="Card image cap" ></img> */}
                 <CardTitle tag="h5" section className="cat"></CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted" h3 className="cat__name">{cat.name}</CardSubtitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted" h3 className="cat__name">
                 <Link to={`/cats/detail/${cat.id}`}>
                     {cat.name}
-                </Link>
+                </Link></CardSubtitle>
                 <address className="cat__zip">{cat.zip}</address>
             </CardBody>
         </Card>
